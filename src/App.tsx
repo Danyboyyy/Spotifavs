35b7d07f8d5f8c2f16d076ff5582c9889ded1e57
Daniel David <danielalejandro.2000@outlook.com>
2020-10-14 19:05:13 -0500

    Fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { getAccessToken } from './requests/GetHashParams';
+import { getAccessToken } from './requests/getAccessToken';
 
 function App() {
   const [user, setUser] = useState('');
+  const [userName, setUserName] = useState('');
+  const [favArtist, setFavArtist] = useState('');
+  const [favGenre, setFavGenre] = useState('');
  
   useEffect(() => {
     const accessToken = getAccessToken();
     setUser(accessToken);
+
+    fetch('https://api.spotify.com/v1/me/following?type=artist', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    })
+    .then((response) => response.json())
+    .then(data => console.log(data.artists.items[0].name))
   });
 
   return (
@@ -19,7 +28,7 @@ function App() {
         : 'https://better-playlists-backend.herokuapp.com/login' }
       }>Sign in with Spotify</button> :
       <div>
-        AAAA
+        {userName}
       </div>
       }
     </div>
